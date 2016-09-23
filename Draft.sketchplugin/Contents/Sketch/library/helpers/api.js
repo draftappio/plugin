a1eb80767d313d8f5a37b70e43195da59a602a64
Islam Wazery <wazery@ubuntu.com>
2016-09-24 01:08:26 +0200

    Implement OAuth 2 basic authentication

--- Draft.sketchplugin/Contents/Sketch/library/helpers/api.js
+++ Draft.sketchplugin/Contents/Sketch/library/helpers/api.js
@@ -2,12 +2,6 @@ var api = new Api();
 
 function Api () {}
 
-// TODO: Implement OAuth 2.0 token auth
-
-// ---------------------------------------- //
-//                  Core API                //
-// ---------------------------------------- //
-
 Api.prototype.post = function(path, params) {
   logger.info("api.post()");
   return this.request("POST", path, params);
@@ -18,37 +12,51 @@ Api.prototype.get = function(path, params) {
   return this.request("GET", path, params);
 };
 
-// Api.prototype.upload = function(context, urls, exportData) {
-//   logger.info("api.upload()");
-//   if (!urls && !exportData) return false;
-//   var bundlePath = exportData[0],
-//   schemaPath = exportData[1];
-//   return (api.uploadWithFilePath(context,urls.signedZippedUploadUrl, bundlePath)) && (api.uploadWithFilePath(context,urls.signedSchemaUploadUrl, schemaPath));
-// };
+Api.prototype.upload = function(context, urls, exportData) {
+  logger.info("api.upload()");
+
+  if (!urls && !exportData) return false;
+
+  var bundlePath = exportData[0],
+  schemaPath = exportData[1];
+
+  return (api.uploadWithFilePath(context, urls.signedZippedUploadUrl, bundlePath)) &&
+    (api.uploadWithFilePath(context, urls.signedSchemaUploadUrl, schemaPath));
+};
 
 Api.prototype.request = function(method, path, params) {
   logger.info("api.request()" + " type: " + method + " to path: " + path);
 
   var url = DraftApp.apiURL + path;
-  logger.debug("Requesting : " + url);
+  logger.debug("Requesting: " + url);
 
   var req = NSMutableURLRequest.requestWithURL(NSURL.URLWithString(url));
   [req setHTTPMethod:method];
   [req setValue:"Sketch" forHTTPHeaderField:"User-Agent"];
 
-  //Add Custom Header
+  // Request Headers
   // [req setValue:utils.sketchVersion forHTTPHeaderField:"X-Sketch-Version"];
   // [req setValue:utils.version forHTTPHeaderField:"X-Sketch-Plugin-Version"];
   [req setValue:"application/json" forHTTPHeaderField:"Content-Type"];
 
-  var token = DraftApp.readAccessToken();
-  if (token) {
+  var authHeaders = DraftApp.readAuthHeaders();
+  if (authHeaders) {
+    var token = authHeaders.accessToken;
+    var client = authHeaders.client;
+    var expiry = authHeaders.expiry;
+    var uid = authHeaders.uid;
+
     logger.debug("Token found");
-    [req setValue:"0fngwoKbczb_dg_C6sGkqQ" forHTTPHeaderField:"access-token"];
+    logger.debug("Token: " + token);
+    logger.debug("client: " + client);
+    logger.debug("expiry: " + expiry);
+    logger.debug("uid: " + uid);
+
+    [req setValue:token forHTTPHeaderField:"access-token"];
     [req setValue:"Bearer" forHTTPHeaderField:"token-type"];
-    [req setValue:"-Sz_6RfEKyUxchjp97qBMQ" forHTTPHeaderField:"client"];
-    [req setValue:"1474263658" forHTTPHeaderField:"expiry"];
-    [req setValue:"wazery@ubuntu.com" forHTTPHeaderField:"uid"];
+    [req setValue:client forHTTPHeaderField:"client"];
+    [req setValue:expiry forHTTPHeaderField:"expiry"];
+    [req setValue:uid forHTTPHeaderField:"uid"];
   }
 
   if (params) {
@@ -61,38 +69,51 @@ Api.prototype.request = function(method, path, params) {
     error = MOPointer.alloc().init(),
     res = NSURLConnection.sendSynchronousRequest_returningResponse_error(req, response, error);
 
-  //Handle 401 - workaround
+  // Handle 401 - workaround
   if (error.value() != nil && api.is401(error) && res != nil){
     var dataString = NSString.alloc().initWithData_encoding(res, NSUTF8StringEncoding);
     logger.error(dataString);
     var json = JSON.parse(dataString);
-    // if(path != 'login') {
+    if(path != 'auth/sign_in') {
       // utils.dealWithNetErrors(context,res);
-      // utils.setActiveTokenOnComputer(nil);
-      // utils.setEmailOnComputer(nil);
-      // if (!path.match(/^upload-url/)){
-        // view.createLoginView(context);
+      DraftApp.resetAccessToken();
+      // TODO: if (!path.match(/^upload-url/)){
+      DraftApp.loginPanel();
       // }
       // return false;
-    // }
+    }
     return json;
   }
 
-  //Handle other status code
-  if(error.value() == nil && res != nil){
+  // Handle other status codes
+  if (error.value() == nil && res != nil) {
     var statusCode = [[response value] statusCode];
     logger.info("Response code: " + statusCode);
-    if(statusCode != 200 && res) {
+
+    if (statusCode != 200 && res) {
       // utils.dealWithNetErrors(context, res); //Alert Error if status code is not 200
-      if(statusCode == 400 || statusCode == 440){
-        // utils.setActiveTokenOnComputer(nil);
-        // utils.setEmailOnComputer(nil);
-        // view.createLoginView(context);
+      if (statusCode == 400 || statusCode == 440) {
+        DraftApp.resetAccessToken();
+        DraftApp.loginPanel();
       }
     }
+
     var dataString = NSString.alloc().initWithData_encoding(res, NSUTF8StringEncoding);
-    logger.debug(dataString);
+    // Parse auth headers and save them
+    logger.debug("Response data: " + dataString);
+
+    var headerFields = [[response value] allHeaderFields];
+
+    var authHeaders = {};
+    authHeaders.accessToken = headerFields["access-token"];
+    authHeaders.client = headerFields["client"];
+    authHeaders.expiry = headerFields["expiry"];
+    authHeaders.uid = headerFields["uid"];
+
+    DraftApp.saveAuthHeaders(authHeaders);
+
     return JSON.parse(dataString);
+
   } else {
     logger.error("error");
     // utils.dealWithNetErrors(context);
@@ -100,37 +121,7 @@ Api.prototype.request = function(method, path, params) {
   }
 };
 
-// Api.prototype.uploadWithFilePath = function(context, url, filePath) {
-//   logger.info("api.uploadWithFilePath() : "+ filePath);
-//   var error = MOPointer.alloc().initWithValue(nil);
-//   var fileData = [NSData dataWithContentsOfFile:filePath options:8 error:error];
-//   utils.uploadBundleSize = [fileData length];
-//
-//   if(!fileData) {
-//     logger.error("failed to read data: " + error.value());
-//     return;
-//   }
-//   var request = NSMutableURLRequest.requestWithURL(NSURL.URLWithString(url));
-//   [request setHTTPMethod:"PUT"];
-//   var body = [NSMutableData data];
-//   [body appendData:fileData];
-//   [request setHTTPBody:body];
-//   var response = MOPointer.alloc().init();
-//   var error = MOPointer.alloc().init();
-//   var res = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, response, error);
-//   var dataString = NSString.alloc().initWithData_encoding(res, NSUTF8StringEncoding);
-//   logger.debug(dataString)
-//   if (error.value() == nil && res != nil) {
-//     var statusCode = [[response value] statusCode];
-//     return statusCode == 200;
-//   } else {
-//     // utils.dealWithNetErrors(context,res);
-//     return false;
-//   }
-//
-// };
-
-//Workaround for 401 error: http://stackoverflow.com/questions/3912532/ios-how-can-i-receive-http-401-instead-of-1012-nsurlerrorusercancelledauthenti
+// Workaround for 401 error: http://goo.gl/GE3LNm
 Api.prototype.is401 = function( error ){
   logger.info("api.is401()");
   if (error == nil) return false;
@@ -139,7 +130,43 @@ Api.prototype.is401 = function( error ){
   if ([strError rangeOfString:@"Code=-1012"].location != NSNotFound) {
     logger.error("401 : Unauthorization");
     return true;
-  }else{
+  } else {
     return false;
   }
+};
+
+Api.prototype.uploadWithFilePath = function(context, url, filePath) {
+  logger.info("api.uploadWithFilePath() : "+ filePath);
+
+  var error = MOPointer.alloc().initWithValue(nil);
+  var fileData = [NSData dataWithContentsOfFile:filePath options:8 error:error];
+  // utils.uploadBundleSize = [fileData length];
+
+  if(!fileData) {
+    logger.error("failed to read data: " + error.value());
+    return;
+  }
+
+  var request = NSMutableURLRequest.requestWithURL(NSURL.URLWithString(url));
+  [request setHTTPMethod:"PUT"];
+
+  var body = [NSMutableData data];
+  [body appendData:fileData];
+  [request setHTTPBody:body];
+
+  var response = MOPointer.alloc().init();
+  var error = MOPointer.alloc().init();
+  var res = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, response, error);
+  var dataString = NSString.alloc().initWithData_encoding(res, NSUTF8StringEncoding);
+
+  logger.debug(dataString)
+
+  if (error.value() == nil && res != nil) {
+    var statusCode = [[response value] statusCode];
+    return statusCode == 200;
+  } else {
+    // utils.dealWithNetErrors(context,res);
+    return false;
+  }
+
 };
