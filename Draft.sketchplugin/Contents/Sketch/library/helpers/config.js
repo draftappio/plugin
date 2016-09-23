015d5de8d3f0f8a1b2f65b71c9bed60781ee228d
Islam Wazery <wazery@ubuntu.com>
2016-09-24 01:10:12 +0200

    Implement setting auth headers to users Mac settings

--- Draft.sketchplugin/Contents/Sketch/library/helpers/config.js
+++ Draft.sketchplugin/Contents/Sketch/library/helpers/config.js
@@ -28,20 +28,48 @@ DraftApp.extend({
     prefix = this.prefix;
     command.setValue_forKey_onLayer(null, prefix, container)
   },
-  saveAccessToken: function (accessToken) {
-    logger.info('config.saveAccessToken()');
+  saveAuthHeaders: function (data) {
+    logger.info('config.saveAuthHeaders()');
+
+    var accessToken = data.accessToken;
+    var client      = data.client;
+    var expiry      = data.expiry;
+    var uid         = data.uid;
+
+    logger.debug("accessToken: " + accessToken);
+    logger.debug("client: " + client);
+    logger.debug("expiry: " + expiry);
+    logger.debug("uid: " + uid);
+
     [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:"token"];
+    [[NSUserDefaults standardUserDefaults] setObject:client forKey:"client"];
+    [[NSUserDefaults standardUserDefaults] setObject:expiry forKey:"expiry"];
+    [[NSUserDefaults standardUserDefaults] setObject:uid forKey:"uid"];
     [[NSUserDefaults standardUserDefaults] synchronize];
   },
-  readAccessToken: function () {
-    logger.info("config.readAccessToken()");
-    var accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:"token"];
-    return accessToken;
+  readAuthHeaders: function () {
+    logger.info("config.readAuthHeaders()");
+
+    var data = {};
+
+    data.accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:"token"];
+    data.client      = [[NSUserDefaults standardUserDefaults] objectForKey:"client"];
+    data.expiry      = [[NSUserDefaults standardUserDefaults] objectForKey:"expiry"];
+    data.uid         = [[NSUserDefaults standardUserDefaults] objectForKey:"uid"];
+
+    return data;
+  },
+  isAuthHeaderExist: function () {
+    if (!this.readAuthHeaders().accessToken) return false;
+
+    return true;
   },
   resetAccessToken: function () {
     logger.info("config.resetAccessToken()");
-    var accessToken = nil;
-    [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:"token"];
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:"token"];
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:"client"];
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:"expiry"];
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:"uid"];
     [[NSUserDefaults standardUserDefaults] synchronize];
   }
 });
